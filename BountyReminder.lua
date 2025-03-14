f21a1c33a240f55b0499b9a6a6f11f40ac5cd5ba
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-03-14 06:59:23 +0300

    Add files via upload

--- BountyReminder.lua
+++ BountyReminder.lua
@@ -2,8 +2,8 @@
 local addonName = "BountyReminder"
 
 -- Constants
-local ITEM_ID = 227784
-local SPELL_ID = 460831
+local ITEM_ID = 233071
+local SPELL_ID = 473218
 local ICON_TEXTURE = 1064187
 local TEXT = "Use Delver's Bounty!"
 
